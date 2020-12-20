0a9eb25bd8ac33f5451e24c67fa9765ece36e202
IsaacBarden <isaactbarden@gmail.com>
2020-12-19 22:58:11 -0600

    Introduced and debugged network2

--- scripts.py
+++ scripts.py
@@ -1,6 +1,13 @@
 import DataLoader as dl
 training_data, validation_data, test_data = dl.load_data_wrapper()
 training_data = list(training_data)
+validation_data = list(validation_data)
+'''
 import BasicNetwork as network
 net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 1.0, test_data=test_data)
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+'''
+import Chap3Network as network
+net = network.Network([784, 30, 10], cost = network.CrossEntropyCost())
+#net.large_weight_initializer()
+net.SGD(training_data[:1000], 200, 10, 0.1, lmbda = 5.0, evaluation_data=validation_data[:100], monitor_evaluation_accuracy = True, early_stopping_n=15)
